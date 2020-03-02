06ac1025af0e3db158993150b070449def701d0f
vagrant <vagrant@homestead>
2020-03-02 15:45:56 +0000

    Added faker functionality to generate sample data

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Comment::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker -> firstName . ' ' . $faker -> lastName,
+        'comment' => $faker -> sentence (12, true),
+        'likes' => $faker -> numberBetween(-20, 150),
     ];
 });
